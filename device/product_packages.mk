3afd6fa2a2a4c73f4cd0dcede1bacf91f40f9ead
Andreas Schneider <asn@cryptomilk.org>
2020-04-07 12:08:56 +0200

    device: Add fingerprint inscreen service
    Change-Id: Iee8099252c2e65dcd1b1bec3b5becfcbce87145a

--- device/product_packages.mk
+++ device/product_packages.mk
@@ -2,4 +2,5 @@
 
 # FINGERPRINT
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.samsung
+    android.hardware.biometrics.fingerprint@2.1-service.samsung \
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.samsung
