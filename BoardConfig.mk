04ba5af550a272512d69fbfb60d9f922b2d9650f
derfelot <5124143+derfelot@users.noreply.github.com>
2020-04-12 03:16:49 +0200

    board: Add build flag to include Magisk at build-time
    Since we currently cannot override kernel configs from device trees, we have two defconfigs per device:
     * exynos9820-beyond[0|1|2]lte_defconfig without Magisk
     * exynos9820-beyond[0|1|2]lte_magisk_defconfig without Magisk
    
    This way, we can choose a defconfig dynamically based on a build flag.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,11 @@ PRODUCT_PLATFORM := exynos9820
 TARGET_OTA_ASSERT_DEVICE := beyond1,beyond1lte,beyond1ltexx
 
 ### KERNEL
-TARGET_KERNEL_CONFIG := exynos9820-beyond1lte_defconfig
+ifeq ($(WITH_MAGISK),true)
+  TARGET_KERNEL_CONFIG := exynos9820-beyond1lte_magisk_defconfig
+else
+  TARGET_KERNEL_CONFIG := exynos9820-beyond1lte_defconfig
+endif
 
 ### PARTITIONS
 # /proc/partitions shows the size in 1024-byte blocks
