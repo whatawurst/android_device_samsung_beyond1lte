43c822527d738b86119ac204458f0ac85d9509ef
Andreas Schneider <asn@cryptomilk.org>
2020-04-14 17:11:59 +0200

    board: Add device manifest for inscreen fingerprint hal
    Change-Id: I0e1f37d27b278ef020aff9b89b3a91d085686646

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,6 +25,8 @@ TARGET_OTA_ASSERT_DEVICE := beyond1,beyond1lte,beyond1ltexx
 
 TARGET_SPECIFIC_HEADER_PATH += $(DEVICE_PATH)/hardware/include
 
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/device_manifest.xml
+
 ### KERNEL
 ifeq ($(WITH_MAGISK),true)
   TARGET_KERNEL_CONFIG := exynos9820-beyond1lte_magisk_defconfig
