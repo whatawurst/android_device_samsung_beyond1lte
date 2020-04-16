db7ccff2715b553fc43f1c77b94b22fe0d8e567e
Tim Zimmermann <tim@linux4.de>
2020-04-16 16:42:57 +0200

    board: add permissions.xml for inscreen fingerprint

---
+++ device/permissions.mk
@@ -0,0 +1,5 @@
+### BIOMETRICS
+
+# FINGERPRINT
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
